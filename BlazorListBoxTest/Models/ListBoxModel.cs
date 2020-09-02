744edf70d6e67c958b9fdc1152cc6854cda0080a
Swdvader <richiep@optonline.net>
2020-09-02 14:23:23 -0400

    Add project files.

---
+++ BlazorListBoxTest/Models/ListBoxModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorListBoxTest
+{
+    public class ListBoxModel
+    {
+        public string Code { get; set; }
+        public string DisplayValue { get; set; }
+    }
+}
+
+
+
